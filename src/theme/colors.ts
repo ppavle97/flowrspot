1745fa4f6fa3a6c4c130b8b77dc9b67fbf6f3661
FIRST_NAME LAST_NAME <ppavle@live.com>
2024-05-18 16:44:23 +0200

    Theme folder with styling configuration

---
+++ src/theme/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  white: "#ffffff", // White
+  lightCoral: "#EAA89F", // Primary Pink
+  cadetGray: "#949EA0", // Silver
+  lightGray: "#DFE5EA", // Light Silver
+  gunmetal: "#334144", // Black
+};
